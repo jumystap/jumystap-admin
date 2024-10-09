a8657ac4831f7d48f4955e9c02937274aea995d5
aidosgal <agalimzhan928@gmail.com>
2024-10-09 12:44:56 +0500

    fix: update the loading animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,11 +17,14 @@ export default function Home() {
     companies_responded_count: number;
   } | null>(null);
 
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const fetchAnalytics = async () => {
       try {
         const response = await axios.get("https://api.jumystap.kz/api/v1/analytics");
         setAnalytics(response.data.analytics);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching analytics:", error);
       }
@@ -29,10 +32,6 @@ export default function Home() {
 
     fetchAnalytics();
   }, []);
-
-  if (!analytics) {
-    return <div className="py-5">Загрузка статистики...</div>;
-  }
 
   return (
     <div className="grid grid-cols-10">
@@ -60,59 +59,74 @@ export default function Home() {
                 Обновить
             </div>
         </div>
-        <div className='grid grid-cols-3 gap-2 mt-3'>
-            <div className='bg-white w-full rounded-lg border border-gray-100'>
-                <div className='text-gray-500 text-sm mt-3 px-5'>Количество пользователей</div> 
-                <div className='flex mt-6 px-5'>
-                    <div>
-                        <div className='text-4xl'>{analytics.user_count}</div>
-                    </div>
-                    <div className='ml-auto'>
-                        <div className='gap-x-3 items-center bg-green-100 ml-auto flex px-2 text-green-700 rounded-lg py-1'>
-                            <FaArrowTrendUp />
-                            <div>2.15%</div>
+        {!loading ? (
+            <div className='grid grid-cols-3 gap-2 mt-3'>
+                <div className='bg-white w-full rounded-lg border border-gray-100'>
+                    <div className='text-gray-500 text-sm mt-3 px-5'>Количество пользователей</div> 
+                    <div className='flex mt-6 px-5'>
+                        <div>
+                            <div className='text-4xl'>{analytics.user_count}</div>
+                        </div>
+                        <div className='ml-auto'>
+                            <div className='gap-x-3 items-center bg-green-100 ml-auto flex px-2 text-green-700 rounded-lg py-1'>
+                                <FaArrowTrendUp />
+                                <div>2.15%</div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className='bg-gray-100 text-sm px-5 py-3 mt-5 text-gray-500'>
-                    <div>С Авг 03, 2024 - Окт 08, 2024</div> 
-                </div>
-            </div>
-            <div className='bg-white w-full rounded-lg border border-gray-100'>
-                <div className='text-gray-500 text-sm mt-3 px-5'>Количество вакансии</div> 
-                <div className='flex mt-6 px-5'>
-                    <div>
-                        <div className='text-4xl'>{analytics.announcements_count}</div>
+                    <div className='bg-gray-100 text-sm px-5 py-3 mt-5 text-gray-500'>
+                        <div>С Авг 03, 2024 - Окт 08, 2024</div> 
                     </div>
-                    <div className='ml-auto'>
-                        <div className='gap-x-3 items-center bg-red-100 ml-auto flex px-2 text-red-700 rounded-lg py-1'>
-                            <FaArrowTrendDown />
-                            <div>1.25%</div>
+                </div>
+                <div className='bg-white w-full rounded-lg border border-gray-100'>
+                    <div className='text-gray-500 text-sm mt-3 px-5'>Количество вакансии</div> 
+                    <div className='flex mt-6 px-5'>
+                        <div>
+                            <div className='text-4xl'>{analytics.announcements_count}</div>
+                        </div>
+                        <div className='ml-auto'>
+                            <div className='gap-x-3 items-center bg-red-100 ml-auto flex px-2 text-red-700 rounded-lg py-1'>
+                                <FaArrowTrendDown />
+                                <div>1.25%</div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className='bg-gray-100 text-sm px-5 py-3 mt-5 text-gray-500'>
-                    <div>С Авг 03, 2024 - Окт 08, 2024</div> 
-                </div>
-            </div>
-            <div className='bg-white w-full rounded-lg border border-gray-100'>
-                <div className='text-gray-500 text-sm mt-3 px-5'>Количество откликов</div> 
-                <div className='flex mt-6 px-5'>
-                    <div>
-                        <div className='text-4xl'>{analytics.responses_count}</div>
+                    <div className='bg-gray-100 text-sm px-5 py-3 mt-5 text-gray-500'>
+                        <div>С Авг 03, 2024 - Окт 08, 2024</div> 
                     </div>
-                    <div className='ml-auto'>
-                        <div className='gap-x-3 items-center bg-green-100 ml-auto flex px-2 text-green-700 rounded-lg py-1'>
-                            <FaArrowTrendUp />
-                            <div>5.15%</div>
+                </div>
+                <div className='bg-white w-full rounded-lg border border-gray-100'>
+                    <div className='text-gray-500 text-sm mt-3 px-5'>Количество откликов</div> 
+                    <div className='flex mt-6 px-5'>
+                        <div>
+                            <div className='text-4xl'>{analytics.responses_count}</div>
+                        </div>
+                        <div className='ml-auto'>
+                            <div className='gap-x-3 items-center bg-green-100 ml-auto flex px-2 text-green-700 rounded-lg py-1'>
+                                <FaArrowTrendUp />
+                                <div>5.15%</div>
+                            </div>
                         </div>
                     </div>
+                    <div className='bg-gray-100 text-sm px-5 py-3 mt-5 text-gray-500'>
+                        <div>С Авг 03, 2024 - Окт 08, 2024</div> 
+                    </div>
                 </div>
-                <div className='bg-gray-100 text-sm px-5 py-3 mt-5 text-gray-500'>
-                    <div>С Авг 03, 2024 - Окт 08, 2024</div> 
+                <div className='col-span-2 py-2 rounded-lg border border-gray-100 bg-white'>
+                    <div
+                        className='px-5'
+                    >
+                        Топ объявлений
+                    </div>
                 </div>
             </div>
-        </div>
+        ) : (
+            <div className="grid grid-cols-3 gap-2 mt-3">
+                <div className="bg-gray-100 animate-pulse w-full rounded-lg h-36"></div>
+                <div className="bg-gray-100 animate-pulse w-full rounded-lg h-36"></div>
+                <div className="bg-gray-100 animate-pulse w-full rounded-lg h-36"></div>
+            </div>
+        )}
       </div>
       <div className='col-span-2 py-5'>
         <div className='bg-white w-full px-5 py-4 border border-gray-100 rounded-lg'>
